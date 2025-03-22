25430885cd8a539a9df4656ab3626266a42f75e0
Valeria Arenas <126576318+ValeriaArenasB@users.noreply.github.com>
2025-03-21 21:17:43 -0500

    feature: basic blog page
    - scroll-reveal doesn't work here either

--- ClinicaShiba/src/app/app-routing.module.ts
+++ ClinicaShiba/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SpecialtiesComponent } from './pages/specialties/specialties.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'specialties', component: SpecialtiesComponent },
+  { path: 'blog', component: BlogComponent },
   { path: '**', redirectTo: '' }, // Redirección en caso de rutas no existentes
 ];
 
