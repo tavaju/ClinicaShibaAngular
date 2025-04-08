eae0232c108c5a22c3d79b0b2ab62aa36a50fe8b
TatisVivas <t_vivas@javeriana.edu.co>
2025-04-07 21:27:55 -0500

    refactor: Landing page cta routing

--- ClinicaShiba/src/app/app-routing.module.ts
+++ ClinicaShiba/src/app/app-routing.module.ts
@@ -9,17 +9,23 @@ import { PetListComponent } from './pages/pets/pet-list/pet-list.component';
 import { PetFormComponent } from './pages/pets/pet-form/pet-form.component';
 import { PetDetailComponent } from './pages/pets/pet-detail/pet-detail.component';
 
+// Import CTA component
+import { CtaComponent } from './pages/home/cta/cta.component';
+
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'specialties', component: SpecialtiesComponent },
   { path: 'blog', component: BlogComponent },
-  
+
   // Pet routes
   { path: 'pets', component: PetListComponent },
   { path: 'pets/add', component: PetFormComponent },
   { path: 'pets/edit/:id', component: PetFormComponent },
   { path: 'pets/:id', component: PetDetailComponent },
   
+  // CTA route within Home
+  { path: 'cta', component: CtaComponent },
+
   // Always put the wildcard route last
   { path: '**', redirectTo: '' }
 ];
