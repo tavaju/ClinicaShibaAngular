1b1ce5c2262c7184ce827a53b21b85b0b584152e
TatisVivas <t_vivas@javeriana.edu.co>
2025-05-25 15:51:06 -0500

    fix: map

--- ClinicaShiba/src/app/pages/pickup-selection/pickup-selection/pickup-selection.component.ts
+++ ClinicaShiba/src/app/pages/pickup-selection/pickup-selection/pickup-selection.component.ts
@@ -1,6 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { Map, tileLayer, latLng, marker, icon, Marker } from 'leaflet';
-
+import { Component, ViewEncapsulation, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 import { Router } from '@angular/router';
 
 interface PickupPoint {
@@ -15,7 +14,7 @@ interface PickupPoint {
   styleUrls: ['./pickup-selection.component.css'],
   encapsulation: ViewEncapsulation.None,
 })
-export class PickupSelectionComponent {
+export class PickupSelectionComponent implements AfterViewInit {
   pickupPoints: PickupPoint[] = [
     { lat: 4.6482837, lng: -74.2478948, address: 'Calle 80 # 69-70, Bogotá' },
     { lat: 4.6097102, lng: -74.081749, address: 'Cra 7 # 32-16, Bogotá' },
@@ -24,37 +23,34 @@ export class PickupSelectionComponent {
   selectedPoint: PickupPoint | null = null;
   selectedDate: string = '';
   selectedTime: string = '';
-
-  options = {
-    layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 18,
-        attribution: '© OpenStreetMap contributors',
-      }),
-    ],
-    zoom: 12,
-    center: latLng(4.6482837, -74.2478948),
-  };
-
-  pickupMarkers: Marker[] = [];
+  private map: L.Map | null = null;
 
   constructor(private router: Router) {}
 
-  onMapReady(event: any) {
-    setTimeout(() => {
-      const map = event as Map;
-      this.pickupMarkers = this.pickupPoints.map((point) =>
-        marker([point.lat, point.lng], {
-          icon: icon({
-            iconUrl: 'https://cdn-icons-png.flaticon.com/512/684/684908.png',
-            iconSize: [32, 32],
-            iconAnchor: [16, 32],
-          }),
-        }).on('click', () => this.selectPoint(point))
-      );
-      this.pickupMarkers.forEach((m) => m.addTo(map));
-      map.invalidateSize(); // <-- fuerza el render
-    }, 100);
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([4.6482837, -74.2478948], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap contributors',
+    }).addTo(this.map);
+
+    // Agregar marcadores
+    this.pickupPoints.forEach((point) => {
+      L.marker([point.lat, point.lng], {
+        icon: L.icon({
+          iconUrl: 'https://cdn-icons-png.flaticon.com/512/684/684908.png',
+          iconSize: [32, 32],
+          iconAnchor: [16, 32],
+        }),
+      })
+        .addTo(this.map!)
+        .on('click', () => this.selectPoint(point));
+    });
   }
 
   selectPoint(point: PickupPoint) {
